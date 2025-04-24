af63d53a0523457b0e9dd3a4f8f1ade8c15de0a8
mywyau <47248104+mywyau@users.noreply.github.com>
2025-04-24 14:22:14 +0100

    add auth rotues

--- src/main/scala/middleware/JwksKeyProvider.scala
+++ src/main/scala/middleware/JwksKeyProvider.scala
@@ -2,21 +2,40 @@ package middleware
 
 import cats.effect._
 import cats.syntax.all._
+import com.auth0.jwt.interfaces.RSAKeyProvider
 import io.circe._
 import io.circe.parser._
-import org.http4s.client.Client
-import com.auth0.jwt.interfaces.RSAKeyProvider
 import java.security.interfaces.RSAPublicKey
 import java.security.spec.RSAPublicKeySpec
 import java.security.KeyFactory
 import java.util.Base64
+import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
+import org.http4s.client.Client
 
-class JwksKeyProvider[F[_]: Sync](jwksUrl: String, client: Client[F]) extends RSAKeyProvider {
+class JwksKeyProvider[F[_] : Concurrent](jwksUrl: String, client: Client[F]) extends RSAKeyProvider {
 
   private var cache: Map[String, RSAPublicKey] = Map.empty
 
   private def base64UrlDecode(s: String): Array[Byte] =
     Base64.getUrlDecoder.decode(s)
+
+  def loadJwks[F[_] : Concurrent](jwksUrl: String, client: Client[F]): F[Map[String, RSAPublicKey]] =
+    client.expect[String](jwksUrl).flatMap { body =>
+      parse(body).flatMap(_.hcursor.downField("keys").as[List[Json]]) match {
+        case Left(err) => Concurrent[F].raiseError(new RuntimeException(s"JWKS parse error: $err"))
+        case Right(keys) =>
+          keys
+            .flatMap { json =>
+              for {
+                kid <- json.hcursor.get[String]("kid").toOption
+                n <- json.hcursor.get[String]("n").toOption
+                e <- json.hcursor.get[String]("e").toOption
+              } yield kid -> buildPublicKey(n, e)
+            }
+            .toMap
+            .pure[F]
+      }
+    }
 
   private def buildPublicKey(n: String, e: String): RSAPublicKey = {
     val spec = new RSAPublicKeySpec(
@@ -30,7 +49,72 @@ class JwksKeyProvider[F[_]: Sync](jwksUrl: String, client: Client[F]) extends RS
   private def fetchKeys: F[Map[String, RSAPublicKey]] =
     client.expect[String](jwksUrl).flatMap { body =>
       parse(body).flatMap(_.hcursor.downField("keys").as[List[Json]]) match {
-        case Left(err) => Sync[F].raiseError(new RuntimeException(s"JWKS parse error: $err"))
+        case Left(err) => Concurrent[F].raiseError(new RuntimeException(s"JWKS parse error: $err"))
+        case Right(keys) =>
+          keys
+            .flatMap { json =>
+              for {
+                kid <- json.hcursor.get[String]("kid").toOption
+                n <- json.hcursor.get[String]("n").toOption
+                e <- json.hcursor.get[String]("e").toOption
+              } yield kid -> buildPublicKey(n, e)
+            }
+            .toMap
+            .pure[F]
+      }
+    }
+    
+  override def getPublicKeyById(keyId: String): RSAPublicKey = ???
+
+  def getPublicKeyByIdF(kid: String): F[RSAPublicKey] =
+    cache.get(kid) match {
+      case Some(key) => Concurrent[F].pure(key)
+      case None =>
+        for {
+          keys <- fetchKeys
+          _ <- Concurrent[F].pure { cache = keys }
+          key <-
+            keys
+              .get(kid)
+              .fold(
+                Concurrent[F].raiseError[RSAPublicKey](
+                  new RuntimeException(s"Unknown KID: $kid")
+                )
+              )(Concurrent[F].pure)
+        } yield key
+    }
+
+  override def getPrivateKey = null
+  override def getPrivateKeyId = null
+}
+
+
+class StaticJwksKeyProvider(keys: Map[String, RSAPublicKey]) extends RSAKeyProvider {
+  override def getPublicKeyById(kid: String): RSAPublicKey =
+    keys.getOrElse(kid, throw new RuntimeException(s"Unknown KID: $kid"))
+
+  override def getPrivateKey = null
+  override def getPrivateKeyId = null
+}
+
+
+object JwksKeyProvider {
+  private def base64UrlDecode(s: String): Array[Byte] =
+    Base64.getUrlDecoder.decode(s)
+
+  private def buildPublicKey(n: String, e: String): RSAPublicKey = {
+    val spec = new RSAPublicKeySpec(
+      new java.math.BigInteger(1, base64UrlDecode(n)),
+      new java.math.BigInteger(1, base64UrlDecode(e))
+    )
+    val factory = KeyFactory.getInstance("RSA")
+    factory.generatePublic(spec).asInstanceOf[RSAPublicKey]
+  }
+
+  def loadJwks[F[_]: Concurrent](jwksUrl: String, client: Client[F]): F[Map[String, RSAPublicKey]] =
+    client.expect[String](jwksUrl).flatMap { body =>
+      parse(body).flatMap(_.hcursor.downField("keys").as[List[Json]]) match {
+        case Left(err) => Concurrent[F].raiseError(new RuntimeException(s"JWKS parse error: $err"))
         case Right(keys) =>
           keys
             .flatMap { json =>
@@ -44,14 +128,4 @@ class JwksKeyProvider[F[_]: Sync](jwksUrl: String, client: Client[F]) extends RS
             .pure[F]
       }
     }
-
-  override def getPublicKeyById(kid: String): RSAPublicKey =
-    cache.getOrElse(kid, {
-      val keys = fetchKeys.unsafeRunSync() // You may replace this with memoization later
-      cache = keys
-      keys.getOrElse(kid, throw new RuntimeException("KID not found in JWKS"))
-    })
-
-  override def getPrivateKey = null
-  override def getPrivateKeyId = null
 }
