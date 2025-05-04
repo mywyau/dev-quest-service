fb6520a1fe8b49af4c51d68bb7f7c90800a5ab8a
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-05-04 17:53:52 +0100

    wip add test for post create quest

--- it/src/test/scala/controllers/quest/QuestControllerISpec.scala
+++ it/src/test/scala/controllers/quest/QuestControllerISpec.scala
@@ -2,30 +2,30 @@ package controllers.quest
 
 import cats.effect.*
 import controller.fragments.QuestControllerFragments.*
-import controllers.ControllerISpecBase
 import controllers.constants.QuestControllerConstants.*
+import controllers.ControllerISpecBase
 import doobie.implicits.*
 import doobie.util.transactor.Transactor
-import io.circe.Json
 import io.circe.syntax.*
-import models.InProgress
+import io.circe.Json
+import java.time.LocalDateTime
 import models.database.*
+import models.quests.CreateQuestPartial
 import models.quests.QuestPartial
 import models.responses.CreatedResponse
 import models.responses.DeletedResponse
 import models.responses.UpdatedResponse
+import models.InProgress
 import org.http4s.*
-import org.http4s.Method.*
 import org.http4s.circe.*
 import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
 import org.http4s.implicits.*
-import org.typelevel.log4cats.SelfAwareStructuredLogger
+import org.http4s.Method.*
 import org.typelevel.log4cats.slf4j.Slf4jLogger
+import org.typelevel.log4cats.SelfAwareStructuredLogger
 import shared.HttpClientResource
 import shared.TransactorResource
 import weaver.*
-
-import java.time.LocalDateTime
 
 class QuestControllerISpec(global: GlobalRead) extends IOSuite with ControllerISpecBase {
 
@@ -106,31 +106,40 @@ class QuestControllerISpec(global: GlobalRead) extends IOSuite with ControllerIS
     }
   }
 
-//   test(
-//     "POST - /dev-quest-service/business/businesses/address/details/create - " +
-//       "should generate the business address data for a business in database table, returning Created response"
-//   ) { (transactorResource, log) =>
+  test(
+    "POST - /dev-quest-service/quest/create - " +
+      "should generate the business address data for a business in database table, returning Created response"
+  ) { (transactorResource, log) =>
 
-//     val transactor = transactorResource._1.xa
-//     val client = transactorResource._2.client
+    val transactor = transactorResource._1.xa
+    val client = transactorResource._2.client
 
-//     val businessAddressRequest: Json = testQuestRequest("user_id_3", "business_id_3").asJson
+    def testCreateQuest(userId: String, questId: String): CreateQuestPartial =
+      CreateQuestPartial(
+        userId = userId,
+        questId = questId,
+        title = "Implement User Authentication",
+        description = Some("Set up Auth0 integration and secure routes using JWT tokens."),
+        status = Some(InProgress)
+      )
 
-//     val request =
-//       Request[IO](POST, uri"http://127.0.0.1:9999/dev-quest-service/business/businesses/address/details/create")
-//         .withEntity(businessAddressRequest)
+    val businessAddressRequest: Json = testCreateQuest("user_id_3", "quest_id_3").asJson
 
-//     val expectedBody = CreatedResponse(CreateSuccess.toString, "Business address details created successfully")
+    val request =
+      Request[IO](POST, uri"http://127.0.0.1:9999/dev-quest-service/quest/create")
+        .withEntity(businessAddressRequest)
 
-//     client.run(request).use { response =>
-//       response.as[CreatedResponse].map { body =>
-//         expect.all(
-//           response.status == Status.Created,
-//           body == expectedBody
-//         )
-//       }
-//     }
-//   }
+    val expectedBody = CreatedResponse(CreateSuccess.toString, "quest details created successfully")
+
+    client.run(request).use { response =>
+      response.as[CreatedResponse].map { body =>
+        expect.all(
+          response.status == Status.Created,
+          body == expectedBody
+        )
+      }
+    }
+  }
 
 //   test(
 //     "PUT - /dev-quest-service/business/businesses/address/details/update/business_id_4 - " +
