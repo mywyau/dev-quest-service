b332c7c2390cbe28443a048f838393da57245dc0
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-05-08 02:53:18 +0100

    quest-9 improve redis config

--- src/main/scala/configuration/models/AppConfig.scala
+++ src/main/scala/configuration/models/AppConfig.scala
@@ -3,14 +3,16 @@ package configuration.models
 import pureconfig.generic.derivation.*
 import pureconfig.ConfigReader
 
+case class FeatureSwitches(useDockerHost: Boolean) derives ConfigReader
+
+case class RedisConfig(dockerHost: String, host: String, port: Int) derives ConfigReader
+
 case class ServerConfig(host: String, port: Int) derives ConfigReader
 
 case class PostgresqlConfig(dbName: String, dockerHost: String, host: String, port: Int, username: String, password: String) derives ConfigReader
 
-case class IntegrationSpecConfig(serverConfig: ServerConfig, postgresqlConfig: PostgresqlConfig) derives ConfigReader
+case class LocalConfig(serverConfig: ServerConfig, postgresqlConfig: PostgresqlConfig, redisConfig: RedisConfig) derives ConfigReader
 
-case class LocalConfig(serverConfig: ServerConfig, postgresqlConfig: PostgresqlConfig) derives ConfigReader
-
-case class FeatureSwitches(useDockerHost: Boolean) derives ConfigReader
+case class IntegrationSpecConfig(serverConfig: ServerConfig, postgresqlConfig: PostgresqlConfig, redisConfig: RedisConfig) derives ConfigReader
 
 case class AppConfig(featureSwitches: FeatureSwitches, localConfig: LocalConfig, integrationSpecConfig: IntegrationSpecConfig) derives ConfigReader
