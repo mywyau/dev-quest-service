9f91e06310b028f18925e430e7973848ee1a626d
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-07-02 15:13:35 +0100

    quest-39 Cleaning up quest service and implementing hiscores total level

--- src/main/scala/routes/Routes.scala
+++ src/main/scala/routes/Routes.scala
@@ -94,16 +94,17 @@ object Routes {
     val questRepository = QuestRepository(transactor)
     val userDataRepository = UserDataRepository(transactor)
     val skillDataRepository = SkillDataRepository(transactor)
-    val langaugeRepository = LanguageRepository(transactor)
+    val languageRepository = LanguageRepository(transactor)
     val rewardRepository = RewardRepository(transactor)
 
-    val levelService = LevelService(skillDataRepository, langaugeRepository)
+    val levelService = LevelService(skillDataRepository, languageRepository)
 
     val questCRUDService =
       QuestCRUDService(
         appConfig,
         questRepository,
         userDataRepository,
+        rewardRepository,
         levelService
       )
 
@@ -134,6 +135,20 @@ object Routes {
     val estimateController = EstimateController(estimateService, sessionCache)
 
     estimateController.routes
+  }
+
+  def hiscoreRoutes[F[_] : Concurrent : Temporal : NonEmptyParallel : Async : Logger](
+    transactor: HikariTransactor[F],
+    appConfig: AppConfig
+  ): HttpRoutes[F] = {
+
+    val skillDataRepository = SkillDataRepository(transactor)
+    val languageRepository = LanguageRepository(transactor)
+    val levelService = LevelService(skillDataRepository, languageRepository)
+
+    val hiscoreController = HiscoreController(levelService)
+
+    hiscoreController.routes
   }
 
   def skillRoutes[F[_] : Concurrent : Temporal : NonEmptyParallel : Async : Logger](
